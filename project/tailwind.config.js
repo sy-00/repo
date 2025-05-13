58eb2b56d404aa8d2dab52977784c1ce789b8223
sy-00 <sylwia3p@wp.pl>
2025-05-13 21:10:40 +0200

    feat: created tailwind.config.js manually and set color primary to red (not working in js file)

---
+++ project/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF0000',
+      },
+    },
+  },
+  plugins: [],
+}
