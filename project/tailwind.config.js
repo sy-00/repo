78e4f4323589b5e14f9374e700b05274fcfadd2c
sy-00 <sylwia3p@wp.pl>
2025-05-16 00:23:44 +0200

    Update tailwind.config.js

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,14 +1,1 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#FF0000',
-      },
-    },
-  },
-  plugins: [],
-}
+
